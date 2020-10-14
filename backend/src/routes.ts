699e76793395090a63f729a69ad813af859d9984
jennifertakagi <jennitakagi@gmail.com>
2020-10-14 17:54:13 -0300

    feat: add routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanagesController from './controllers/OrphanagesController'
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanage/:id', OrphanagesController.show);
+
+routes.post('/orphanage', upload.array('images'), OrphanagesController.create);
+
+export default routes;
