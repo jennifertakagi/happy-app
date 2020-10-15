3ea1e5b2cb6972679d172bc28afbc4cbaffb4d93
jennifertakagi <jennitakagi@gmail.com>
2020-10-15 12:48:49 -0300

    feat: add mapIcon utils and api as service

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -6,7 +6,7 @@ import '../assets/styles/pages/landing.css';
 
 import logoIcon from '../assets/icons/logo.svg';
 
-function Landing () {
+function Landing() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
@@ -22,12 +22,12 @@ function Landing () {
           <span>Santa Catarina</span>
         </div>
 
-        <Link to="/orphanages" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
 export default Landing;
