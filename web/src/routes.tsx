8dd959801c5be9e96ae2e863dea1efe199c9b026
jennifertakagi <jennitakagi@gmail.com>
2020-10-15 12:46:57 -0300

    feat: add acios and new routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
-function Routes () {
+function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/orphanages" component={OrphanagesMap} />
+        <Route path="/" component={Landing} exact />
+        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanage/create" component={CreateOrphanage} />
+        <Route path="/orphanage/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default Routes;
